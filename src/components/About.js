98e9b658a2998f49e9b8aab67898777b22a8c8f9
victormuthiora <victormuthiora617@gmail.com>
2024-04-28 14:05:51 +0300

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Links from "./Links";
+
+function About(props) {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      {props.bio && <p>{props.bio}</p>}
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      {/* add your <Links /> component here */}
+      <Links linkedin={props.linkedin} github={props.github} />;
+    </div>
+  );
+}
+
+export default About;
